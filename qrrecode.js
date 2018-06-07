6198de57ed2a37965d667d708c30e55829fc2785
李昕亮 <lixinliang@yy.com>
2018-06-07 10:52:30 +0800

    update qrrecode.js

--- qrrecode.js
+++ qrrecode.js
@@ -1,19 +1,27 @@
 /**
+ * @param  {String} message
+ */
+function log ( message, value ) {
+    console.warn(`[QRRecode]:${ message }`, value);
+}
+
+/**
  * Create a transition animation when reload a new qrcode
  * @param  {HTMLCanvasElement} backgroundCanvas The original canvas and it will be redrawed
  * @param  {HTMLCanvasElement} foregroundCanvas The new canvas with the new qrcode
  * @param  {Object} options Including animation specific options
- * @return {Promise} Resolved when animation done
+ * @return {Promise} Resolved when animation is done
  */
-export default function ( backgroundCanvas, foregroundCanvas, options ) {
-
+function QRRecode ( backgroundCanvas, foregroundCanvas, options ) {
     /**
      * Arguments type checking
      */
     if (!(backgroundCanvas instanceof HTMLCanvasElement)) {
+        log('<HTMLCanvasElement>backgroundCanvas', backgroundCanvas);
         return Promise.resolve();
     }
     if (!(foregroundCanvas instanceof HTMLCanvasElement)) {
+        log('<HTMLCanvasElement>foregroundCanvas', foregroundCanvas);
         return Promise.resolve();
     }
 
@@ -33,11 +41,62 @@ export default function ( backgroundCanvas, foregroundCanvas, options ) {
     const destinationContext = destinationCanvas.getContext('2d');
 
     /**
-     * clear destinationCanvas
+     * Clear destinationCanvas
      */
     function clear () {
         destinationContext.clearRect(0, 0, destinationWidth, destinationHeight);
     }
+
+    /**
+     * Output the last frame
+     * @return {Promise} Resolved when animation is done
+     */
+    function done () {
+        clear();
+        destinationContext.drawImage(
+            foregroundCanvas,
+            0, 0, foregroundWidth, foregroundHeight,
+            0, 0, destinationWidth, destinationHeight,
+        );
+        return Promise.resolve();
+    }
+
+    if (!options) {
+        /**
+         * No animation without options
+         */
+        log('<Object>options', options);
+        return done();
+    }
+
+    /**
+     * @type {Number} duration Animation duration
+     */
+    const duration = +options.duration || 0;
+
+    if (!duration) {
+        /**
+         * No animation without duration
+         */
+        log('<Number>options.duration', duration);
+        return done();
+    }
+
+    /**
+     * @type {String} type Animation name, enum
+     */
+    const type = `${ options.type }`;
+
+    /**
+     * @type {Function} callback A progress callback
+     */
+    const { callback } = options;
+
+    /**
+     * @type {Number} startTime, endTime
+     */
+    const startTime = +new Date;
+    const endTime = startTime + duration;
 
     {
         /**
@@ -49,148 +108,135 @@ export default function ( backgroundCanvas, foregroundCanvas, options ) {
             0, 0, destinationWidth, destinationHeight,
         );
 
-        if (!options) {
-            /**
-             * No animation without options
-             */
-            clear();
-            destinationContext.drawImage(
-                foregroundCanvas,
-                0, 0, foregroundWidth, foregroundHeight,
-                0, 0, destinationWidth, destinationHeight,
-            );
+        /**
+         * @type {Object} closure Save variable
+         */
+        const closure = {};
+
+        if (!fn.hasOwnProperty(type)) {
+            log('<Function>fn[type]', fn);
             return Promise.resolve();
         }
 
-        const type = `${ options.type }`;
-        const duration = +options.duration || 0;
-
-        const startTime = +new Date;
-        const endTime = startTime + duration;
-
-        let size = -1;
-
-        function draw ( percentage ) {
-            clear();
-
-            if (type == 'smooth-fade') {
-                destinationContext.globalAlpha = 1 - percentage;
-                destinationContext.drawImage(
-                    backgroundCanvas,
-                    0, 0, backgroundWidth, backgroundHeight,
-                    0, 0, destinationWidth, destinationHeight,
-                );
-                destinationContext.globalAlpha = percentage;
-                destinationContext.drawImage(
-                    foregroundCanvas,
-                    0, 0, foregroundWidth, foregroundHeight,
-                    0, 0, destinationWidth, destinationHeight,
-                );
-                destinationContext.globalAlpha = 1;
-                return;
-            }
-
-            if (type == 'smooth-blur') {
-                destinationContext.filter = `blur(${ (1 - Math.abs(percentage * 2 - 1)) * 5 }px)`;
-                destinationContext.globalAlpha = 1 - percentage;
-                destinationContext.drawImage(
-                    backgroundCanvas,
-                    0, 0, backgroundWidth, backgroundHeight,
-                    0, 0, destinationWidth, destinationHeight,
-                );
-                destinationContext.globalAlpha = percentage;
-                destinationContext.drawImage(
-                    foregroundCanvas,
-                    0, 0, foregroundWidth, foregroundHeight,
-                    0, 0, destinationWidth, destinationHeight,
-                );
-                destinationContext.globalAlpha = 1;
-                destinationContext.filter = '';
-                return;
-            }
-
-            if (type == 'test') {
-                // todo
-
-                if (size == -1) {
-                    size = 0;
-                    function getPixelSize ( canvas ) {
-                        const context = canvas.getContext('2d');
-                        const { width, height } = canvas;
-                        let size = 1;
-                        while (true) {
-                            const { data } = context.getImageData(size, size, 1, 1);
-                            const color = [];
-                            [].push.apply(color, data);
-                            const [ r, g, b ] = color;
-                            if (r === g && g === b && b === 255) {
-                                return size;
-                            }
-                            size++;
-                            if (size > width) {
-                                return 0;
-                            }
-                            if (size > height) {
-                                return 0;
-                            }
-                        }
-                    }
-                    size = Math.min(getPixelSize(backgroundCanvas), getPixelSize(foregroundCanvas));
-                }
-
-                if (size > 0) {
-                    const total = destinationWidth / size;
-                    for (let x = 0; x < total; x++) {
-                        for (let y = 0; y < total; y++) {
-                            const dx = total - 2 * x;
-                            const dy = total - 2 * y;
-                            destinationContext.globalAlpha = 1 - percentage;
-                            destinationContext.drawImage(
-                                backgroundCanvas,
-                                size * x, size * y, size, size,
-                                size * (x + dx * percentage), size * (y + dy * percentage), size, size,
-                            );
-                            destinationContext.globalAlpha = percentage;
-                            destinationContext.drawImage(
-                                foregroundCanvas,
-                                size * x, size * y, size, size,
-                                size * x, size * y, size, size,
-                            );
-                            destinationContext.globalAlpha = 1;
-                        }
-                    }
-                }
-
-                return;
-            }
-
-            // Waiting for your pull requests
-            // if (type == 'any good animation idea') {
-            // your code
-            // }
-
-            destinationContext.drawImage(
-                foregroundCanvas,
-                0, 0, foregroundWidth, foregroundHeight,
-                0, 0, destinationWidth, destinationHeight,
-            );
-        }
-
+        /**
+         * Proceed the animation
+         * @return {Promise} Resolved when animation is done
+         */
         return new Promise(( resolve ) => {
-            function loop () {
+            const payload = {
+                closure,
+                options,
 
+                backgroundWidth,
+                backgroundHeight,
+                backgroundCanvas,
+
+                foregroundWidth,
+                foregroundHeight,
+                foregroundCanvas,
+
+                destinationWidth,
+                destinationHeight,
+                destinationCanvas,
+                destinationContext,
+            };
+            function next () {
+                clear();
                 const now = +new Date;
-                const percentage = (now - startTime) / (endTime - startTime);
-
+                const percentage = Math.max(0, Math.min((now - startTime) / (endTime - startTime), 1));
+                const call = ( type ) => fn[type].call(payload, percentage, call);
+                call(type);
+                if (callback) {
+                    callback(percentage);
+                }
                 if (percentage >= 1) {
                     resolve();
                 } else {
-                    draw(percentage);
-                    requestAnimationFrame(loop);
+                    requestAnimationFrame(next);
                 }
             }
-            requestAnimationFrame(loop);
+            requestAnimationFrame(next);
         });
     }
-
 };
+
+/**
+ * @type {Object} fn Save animation methods
+ */
+const fn = QRRecode.fn = {};
+
+QRRecode.add = function ( type, method ) {
+    fn[type] = method;
+};
+
+/**
+ * @option type : 'smooth-fade'
+ */
+QRRecode.add('smooth-fade', function ( percentage, call ) {
+    const {
+        backgroundWidth,
+        backgroundHeight,
+        backgroundCanvas,
+
+        foregroundWidth,
+        foregroundHeight,
+        foregroundCanvas,
+
+        destinationWidth,
+        destinationHeight,
+        destinationContext,
+    } = this;
+    destinationContext.globalAlpha = 1 - percentage;
+    destinationContext.drawImage(
+        backgroundCanvas,
+        0, 0, backgroundWidth, backgroundHeight,
+        0, 0, destinationWidth, destinationHeight,
+    );
+    destinationContext.globalAlpha = percentage;
+    destinationContext.drawImage(
+        foregroundCanvas,
+        0, 0, foregroundWidth, foregroundHeight,
+        0, 0, destinationWidth, destinationHeight,
+    );
+    destinationContext.globalAlpha = 1;
+});
+
+/**
+ * @option type : 'smooth-blur'
+ * @option blur ?: Number = 5
+ * @extends smooth-fade
+ */
+QRRecode.add('smooth-blur', function ( percentage, call ) {
+    const {
+        options,
+        destinationContext,
+    } = this;
+    const { blur = 5 } = options;
+    destinationContext.filter = `blur(${ (1 - Math.abs(percentage * 2 - 1)) * blur }px)`;
+    call('smooth-fade');
+    destinationContext.filter = '';
+});
+
+// Waiting for your pull requests
+QRRecode.add('any-good-animation-idea', function ( percentage, call ) {
+    const {
+        closure,
+        options,
+
+        backgroundWidth,
+        backgroundHeight,
+        backgroundCanvas,
+
+        foregroundWidth,
+        foregroundHeight,
+        foregroundCanvas,
+
+        destinationWidth,
+        destinationHeight,
+        destinationCanvas,
+        destinationContext,
+    } = this;
+    // your code
+});
+
+export default QRRecode;
